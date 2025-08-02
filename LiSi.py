e3852cc3ad852472bf4c5b4364a0938706a9464c
Ethan <ethanfeng1114@gmail.com>
2025-08-02 14:47:46 -0400

    Initial commit

---
+++ LiSi.py
@@ -0,0 +1,93 @@
+import os
+import timeit
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pybamm
+from Durdel2023 import get_parameter_values
+
+style.use("ggplot")
+
+
+"Primary Active Material"
+rho_si = 2.336 #g/cm^3 from google
+wt_si = .679 #% from ref
+"Secondary Active Material"
+rho_c6 = 2.255 # (C-NERGYTM KS6L)
+wt_c6 = .199
+"Inactive Material"
+rho_cb = 2.00 #from ref
+wt_cb = 0.02 # from ref
+rho_lipaa = 1.2 #ref
+wt_lipaa = 0.082 # ref
+rho_cmc = 1.6 #guess
+wt_cmc = 0.002 #ref
+
+v_tot  = wt_si/rho_si + wt_c6/rho_c6 + wt_cb/rho_cb + wt_lipaa/rho_lipaa + wt_cmc/rho_cmc
+v_si = wt_si/rho_si
+v_c6 = wt_c6/rho_c6
+v_im = wt_c6/rho_c6 + wt_lipaa/rho_lipaa + wt_cmc/rho_cmc
+
+eps_si = 0.5 * v_si / v_tot #times 0.5 to account for porosity (eps_ely = 0.5)
+eps_c6 = 0.5 * v_c6 / v_tot
+eps_am = 0.5* (v_si + v_c6) / v_tot
+
+print("Volume fraction of Si: ", eps_si
+      , "Volume fraction of C6: ", eps_c6, "Volume fraction of AM: ", eps_am)
+
+
+
+start = timeit.default_timer()
+model = pybamm.lithium_ion.DFN(
+    {"particle phases": ("2", "1"),
+     "open-circuit potential": (("single", "current sigmoid"), "single"),
+    }
+)
+
+param = get_parameter_values()
+
+
+
+#Cycling
+
+experiment = pybamm.Experiment(
+    [
+        (
+            "Discharge at C/10 until 3.0 V",
+            "Charge at C/10 until 4.2 V",
+        ),
+    ]
+)
+
+
+solution = []
+# param.update(
+# {
+#         "Primary: Negative electrode active material volume fraction": (1 - eps_si)
+#         * eps_am,  # primary (graphite)
+#         "Secondary: Negative electrode active material volume fraction": eps_si
+#         * eps_am, # secondary (silicon)
+#      }
+# )
+#????????
+
+
+
+sim = pybamm.Simulation(
+    model,
+    experiment=experiment,
+    parameter_values=param,
+)
+solution = sim.solve()
+stop = timeit.default_timer()
+print("running time: " + str(stop - start) + "s")
+
+plt.figure()
+ltype = ["k-", "r--", "b-.", "g:", "m-", "c--", "y-."]
+
+t_i = solution["Time [s]"].entries / 3600
+V_i = solution["Voltage [V]"].entries
+plt.plot(t_i, V_i, ltype[1], label="$V_\mathrm{si}=$" + str(v_si))
+plt.xlabel("Time [h]")
+plt.ylabel("Voltage [V]")
+plt.legend()
+plt.savefig("LiSi.png")
---
+++ composite.py
@@ -0,0 +1,149 @@
+import os
+import timeit
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pybamm
+
+style.use("ggplot")
+
+start = timeit.default_timer()
+model = pybamm.lithium_ion.DFN(
+    {"particle phases": ("2", "1"),
+     "open-circuit potential": (("single", "current sigmoid"), "single"),
+    }
+)
+
+param = pybamm.ParameterValues("Chen2020_composite")
+
+param.update({"Upper voltage cut-off [V]": 4.5})
+param.update({"Lower voltage cut-off [V]": 2.5})
+
+param.update(
+    {
+        "Primary: Maximum concentration in negative electrode [mol.m-3]": 28700,
+        "Primary: Initial concentration in negative electrode [mol.m-3]": 23000,
+        "Primary: Negative particle diffusivity [m2.s-1]": 5.5e-14,
+        "Secondary: Negative particle diffusivity [m2.s-1]": 1.67e-14,
+        "Secondary: Initial concentration in negative electrode [mol.m-3]": 277000,
+        "Secondary: Maximum concentration in negative electrode [mol.m-3]": 278000,
+    }
+)
+
+C_rate = 0.5
+capacity = param["Nominal cell capacity [A.h]"]
+I_load = C_rate * capacity
+
+t_eval = [0, 10000]
+
+param["Current function [A]"] = I_load
+
+v_si = [0.001, 0.04, 0.1]
+total_am_volume_fraction = 0.75
+solution = []  
+
+for v in v_si:
+    param.update(
+        {
+            "Primary: Negative electrode active material volume fraction": (1 - v)
+            * total_am_volume_fraction,  # primary
+            "Secondary: Negative electrode active material volume fraction": v
+            * total_am_volume_fraction,
+        }
+    )
+    print(v)
+    sim = pybamm.Simulation(
+        model,
+        parameter_values=param,
+    )
+    solution.append(sim.solve(t_eval=t_eval))
+stop = timeit.default_timer()
+print("running time: " + str(stop - start) + "s")
+
+
+ltype = ["k-", "r--", "b-.", "g:", "m-", "c--", "y-."]
+for i in range(0, len(v_si)):
+    t_i = solution[i]["Time [s]"].entries / 3600
+    V_i = solution[i]["Voltage [V]"].entries
+    plt.plot(t_i, V_i, ltype[i], label="$V_\mathrm{si}=$" + str(v_si[i]))
+plt.xlabel("Time [h]")
+plt.ylabel("Voltage [V]")
+plt.legend()
+plt.savefig("composite_ocvs.png")
+
+plt.figure()
+for i in range (0, len(v_si)):
+    t_i = solution[i]["Time [s]"].entries / 3600
+    OCP_i = solution[i]["X-averaged negative electrode primary open-circuit potential [V]"].entries
+    plt.plot(t_i, OCP_i, ltype[i], label="$V_\mathrm{si}=$" + str(v_si[i]))
+plt.xlabel("Time [h]")
+plt.ylabel("OCP [V]")
+plt.legend()
+plt.title("Graphite")
+plt.savefig("graphite_ocp.png")
+
+plt.figure()
+for i in range (0, len(v_si)):
+    t_i = solution[i]["Time [s]"].entries / 3600
+    OCP_i = solution[i]["X-averaged negative electrode secondary open-circuit potential [V]"].entries
+    plt.plot(t_i, OCP_i, ltype[i], label="$V_\mathrm{si}=$" + str(v_si[i]))
+plt.xlabel("Time [h]")
+plt.ylabel("OCP [V]")
+plt.legend()
+plt.title("Silicon")
+plt.savefig("silicon_ocp.png")
+
+plt.figure()
+for i in range (0, len(v_si)):
+    t_i = solution[i]["Time [s]"].entries / 3600
+    OCP_i = solution[i]["X-averaged positive electrode open-circuit potential [V]"].entries
+    plt.plot(t_i, OCP_i, ltype[i], label="$V_\mathrm{si}=$" + str(v_si[i]))
+plt.xlabel("Time [h]")
+plt.ylabel("OCP [V]")
+plt.legend()
+plt.title("NMC811")
+plt.savefig("NMC_ocp.png")
+
+#Cycling
+
+experiment = pybamm.Experiment(
+    [
+        (
+            "Discharge at C/2 until 3.0 V",
+            "Rest for 1 hour",
+            "Charge at C/2 until 4.2 V",
+            "Rest for 1 hour",
+        ),
+    ]
+    * 2
+)
+
+
+solution = []
+for v in v_si:
+    param.update(
+        {
+            "Primary: Negative electrode active material volume fraction": (1 - v)
+            * total_am_volume_fraction,  # primary
+            "Secondary: Negative electrode active material volume fraction": v
+            * total_am_volume_fraction,
+        }
+    )
+    print(v)
+    sim = pybamm.Simulation(
+        model,
+        experiment=experiment,
+        parameter_values=param,
+    )
+    solution.append(sim.solve())
+stop = timeit.default_timer()
+print("running time: " + str(stop - start) + "s")
+
+plt.figure()
+for i in range(0, len(v_si)):
+    t_i = solution[i]["Time [s]"].entries / 3600
+    V_i = solution[i]["Voltage [V]"].entries
+    plt.plot(t_i, V_i, ltype[i], label="$V_\mathrm{si}=$" + str(v_si[i]))
+plt.xlabel("Time [h]")
+plt.ylabel("Voltage [V]")
+plt.legend()
+plt.savefig("composite_cycling_ocvs.png")
